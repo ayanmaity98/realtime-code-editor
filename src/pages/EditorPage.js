069c74f257ce771ca8ba97fd5de3535a565d853c
Debabrata Maity <ayan.maity.1621101@gmail.com>
2023-01-01 22:35:23 +0530

    Initial commit

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,94 @@
+import React, {useState, useRef, useEffect} from 'react';
+import toast from 'react-hot-toast'; 
+import ACTIONS from '../Actions';
+import Client from '../components/Client';
+import Editor from '../components/editor';
+import { initSocket } from '../socket';
+import { useLocation, useNavigate, Navigate, useParams } from 'react-router-dom';
+
+const EditorPage = () => {
+  const socketRef = useRef(null);
+  const location = useLocation();
+  const {roomId} = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+      const init = async () => {
+          socketRef.current = await initSocket();
+          socketRef.current.on('connect_error', (err) => handleErrors(err));
+          socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+          function handleErrors(e) 
+          {
+            console.log('socket error', e);
+            toast.error('Socket connection failed, try again later');
+            reactNavigator('/');
+          }
+          socketRef.current.emit(ACTIONS.JOIN, {
+            roomId,
+            username: location.state?.username,
+          });
+
+          //Listening for joined event
+          socketRef.current.on (
+            ACTIONS.JOINED,
+            ({ clients, username, socketId }) => {
+              if(username !== location.state?.username) {
+                toast.success(`${username} joined the room.`);
+                console.log(`${username} joined`);
+              }
+              setClients(clients);
+            }
+          );
+
+          //Listening for disconnected
+          socketRef.current.on(ACTIONS.DISCONNECTED,  ({ socketId, username }) => {
+              toast.success(`${username} left the room.`);
+              setClients((prev) => {
+                return prev.filter((client) => client.socketId !== socketId);
+              });
+          });
+      };
+      init();
+      return () => {
+        socketRef.current.disconnect();
+        socketRef.current.off(ACTIONS.JOINED);
+        socketRef.current.off(ACTIONS.DISCONNECTED);
+      };
+  }, []);
+
+
+  if(!location.state)
+  {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+         <div className="asideInner">
+           <div className="logo">
+             <img className="logoImage" src="/BlackBox.png" alt="logo" />
+           </div>
+           <h3>Connected</h3>
+           <div className="clientsList">
+              {clients.map((client) => (
+                  <Client 
+                    key={client.socketId} 
+                    username={client.username} 
+                  />
+              ))}
+           </div>
+         </div>
+        <button className="btn copyBtn">Copy ROOM ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorwrap">
+          <Editor />
+      </div>      
+    </div>
+  )
+}
+
+export default EditorPage
---
+++ src/pages/Home.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import {v4 as uuidV4} from 'uuid';
+import toast from 'react-hot-toast';
+import {useNavigate} from 'react-router-dom';
+
+
+const Home = () => {
+    const navigate = useNavigate();
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
+
+    const joinRoom = () => {
+        if(!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
+        }
+        //Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                 username
+            }
+        });
+    };
+
+    const handleInputEnter = (e) => {
+        if(e.code === 'Enter'){
+            joinRoom();
+        }
+    };
+  return (
+    <div className="homePageWrapper">
+        <div className="formWrapper">
+            <img className="homePageLogo" src="/BlackBox.png" alt="code-logo" />
+            <h4 className="mainLabel">Paste Invitation ROOM ID</h4>
+            <div className="inputGroup">
+                <label className="custom-field">
+                    <input type="text" onChange={(e) => setRoomId(e.target.value)} value={roomId} onKeyUp={handleInputEnter} required/> 
+                    <span className="placeholder">ROOM ID</span>
+                </label>                  
+                <label className="custom-field">
+                    <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} onKeyUp={handleInputEnter} required/> 
+                    <span className="placeholder">USERNAME</span>
+                </label>
+                <button className="btn joinBtn" onClick={joinRoom}>Join</button>
+                <span className="createInfo">
+                    If you don't have an invite then create &nbsp;
+                    <a onClick={createNewRoom} href="" className="createNewBtn">new room</a>
+                </span>
+            </div>
+        </div>
+        <footer>
+            <h4>Built with Debabrata Maity</h4>
+        </footer>
+    </div>
+  )
+}
+
+export default Home;
